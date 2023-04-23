7e9b6895c7df28d78349b9354eb2fc17b65ec887
Sabri Can Çetinbaş <scancetinbas@gmail.com>
2023-04-23 23:05:20 +0300

    item details page added

---
+++ DinerMenu/DinerMenu/ItemDetail.swift
@@ -0,0 +1,20 @@
+//
+//  ItemDetail.swift
+//  DinerMenu
+//
+//  Created by Can Cetin on 23.04.2023.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemDetail()
+    }
+}
