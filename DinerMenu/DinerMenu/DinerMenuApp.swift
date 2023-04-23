ad517cdbf1454a484e05b8b833a84f1d7f0ec3d5
Sabri Can Çetinbaş <scancetinbas@gmail.com>
2023-04-23 17:45:57 +0300

    DinerMenu first commit s

---
+++ DinerMenu/DinerMenu/DinerMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinerMenuApp.swift
+//  DinerMenu
+//
+//  Created by Can Cetin on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DinerMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
