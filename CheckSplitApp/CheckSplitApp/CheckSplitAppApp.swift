0c08aeed9647356fbd9144ce9aa874767663498b
Sabri Can Çetinbaş <scancetinbas@gmail.com>
2023-04-29 01:13:58 +0300

    CheckSplitApp initial commit

---
+++ CheckSplitApp/CheckSplitApp/CheckSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSplitAppApp.swift
+//  CheckSplitApp
+//
+//  Created by Can Cetin on 29.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CheckSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
