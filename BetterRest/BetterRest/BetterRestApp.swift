d386f58c00e68f906de5c7a04bb999106318079d
Sabri Can Çetinbaş <scancetinbas@gmail.com>
2023-05-03 18:15:51 +0300

    betterrest initial commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Can Cetin on 3.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
