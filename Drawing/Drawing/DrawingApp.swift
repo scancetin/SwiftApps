8367e76258e9cbb664e4b1bed6ba7dc82a49bd5f
Sabri Can Çetinbaş <scancetinbas@gmail.com>
2023-05-05 01:35:12 +0300

    stickerpack initial

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Can Cetin on 5.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FormulaReflex/FormulaReflex/FormulaReflexApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormulaReflexApp.swift
+//  FormulaReflex
+//
+//  Created by Can Cetin on 5.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FormulaReflexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
